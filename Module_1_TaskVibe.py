c59fbec6eac39e61c02c0934b2c261159d6bbf20
MSTOVALL <melissa.stovall@vizientinc.com>
2026-05-12 16:49:03 -0500

    added functions to task list class

--- Module_1_TaskVibe.py
+++ Module_1_TaskVibe.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 
 class Task:   #individual task class
-    def __init__(self):
+    def __init__(self):  #constructor for the individual task, creates a new task object
 	    self.task_name = "name"
 	    self.task_description ="description"
 	    self.task_status = "Incomplete"
 
-class TaskTracker:  #indi
-    def __init__(self):
-	    self.list_date = "January 1, 2020"
+class TaskTracker:   #task list class 
+    def __init__(self): #constructor for the list of tasks, create a new task list object 
+	    self.task_list_name = "name"
+        self.list_date = "January 1, 2020"
         self.task_item = []		
        	   
 
-    def add_task(self, Task):
+    def add_task(self, Task): #adds a new task only 
+        self.task_item.append(Task)
 	
-	def mark_as_compete(self,name):
+	def mark_as_compete(self,name): #Changes status of task from incomplete to competed
 	
-	def remove_task(self, name):
+	def remove_task(self, name): #Deletes a task 
+        for item in self.task_item
+            if item.task_name == name:
+                self.task_item.remove(item)
 	
-	def view_tasks(self):
+	def view_tasks(self,date): #views all daily tasks 
+        date_task_list =[]
+        for item in self.task_item
+           if item.task_date = date:
+               date_task_list.append(item)
+        
+        for item in date_task_list
+            print(item)        
 
 
 def print_menu(task): #function to create the menu
